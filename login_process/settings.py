3880c40567b978936ea25fcceb7f4facca0f05a6
unknown <you@example.com>
2025-04-03 17:31:41 +0530

    third commit

--- login_process/settings.py
+++ login_process/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%*s637b5129mgm+d)#7=13qh)!u-f@r4f*#to*!-otoidqh(7p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['registration-6hyr.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['registration-6hyr.onrender.com', '127.0.0.1', 'localhost:3000', 'localhost:5000', 'localhost']
 
 import os
 PORT = os.getenv("PORT", "8000")
